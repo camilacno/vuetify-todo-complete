d9141b534ad766d2d2d4d217127f0b160e3d5fa4
Camila Nepomuceno <camila.cno@gmail.com>
2021-02-16 19:27:12 -0300

    Basic About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,32 +1,13 @@
 <template>
   <div class="about pa-6">
     <h1>About {{ $store.state.appTitle }}</h1>
+    <br />
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-      pulvinar orci dictum turpis posuere, eget eleifend felis cursus. Sed
-      auctor sapien non lectus accumsan, vel aliquam arcu blandit. Donec viverra
-      leo nec suscipit tempus. Nunc at ipsum vel enim imperdiet interdum nec ut
-      dui. Donec facilisis pharetra felis, eu pellentesque eros facilisis at.
-      Phasellus elementum sapien vel felis congue, nec tempor libero pretium.
-      Morbi eu sodales lectus. Nunc dapibus ultricies lacus, vitae tempus erat
-      porttitor at. Aliquam at quam euismod, vestibulum urna in, accumsan dui.
-      Integer mollis ex in magna finibus vestibulum. Pellentesque et erat
-      feugiat, ornare tellus sed, aliquam nibh. Quisque dictum posuere aliquam.
-      Fusce vitae malesuada purus.
+      This application was developed by Camila Nepomuceno using Vue and Vuetify.
     </p>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-      pulvinar orci dictum turpis posuere, eget eleifend felis cursus. Sed
-      auctor sapien non lectus accumsan, vel aliquam arcu blandit. Donec viverra
-      leo nec suscipit tempus. Nunc at ipsum vel enim imperdiet interdum nec ut
-      dui.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-      pulvinar orci dictum turpis posuere, eget eleifend felis cursus. Sed
-      auctor sapien non lectus accumsan, vel aliquam arcu blandit. Donec viverra
-      leo nec suscipit tempus. Nunc at ipsum vel enim imperdiet interdum nec ut
-      dui.
-    </p>
+    <p>Contact e-mail: camila.cno@gmail.com</p>
+    <a href="https://github.com/camilacno">Github</a>
+    <br />
+    <a href="https://www.linkedin.com/in/camilacno/">LinkedIn</a>
   </div>
 </template>
